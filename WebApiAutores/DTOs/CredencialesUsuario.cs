1dc1777f3c18947fcbf5cd68b9483b844e6dc1e2
aaron carrizales <aaronacr@gmail.com>
2022-12-01 12:11:14 -0600

    prueba inicial

---
+++ WebApiAutores/DTOs/CredencialesUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApiAutores.DTOs
+{
+    public class CredencialesUsuario
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ WebApiAutores/Middlewares/LoggearRespuestaHTTPMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WebApiAutores.Middlewares
+{
+    public static class LoggearRespuestaHTTPMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoggearRespuestaHTTP(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<LoggearRespuestaHTTPMiddleware>();
+        }
+    }
+
+    public class LoggearRespuestaHTTPMiddleware
+    {
+        private readonly RequestDelegate siguiente;
+        private readonly ILogger<LoggearRespuestaHTTPMiddleware> logger;
+
+        public LoggearRespuestaHTTPMiddleware(RequestDelegate siguiente, ILogger<LoggearRespuestaHTTPMiddleware> logger)
+        {
+            this.siguiente = siguiente;
+            this.logger = logger;
+        }
+        // Invoke o InvokeAsync
+        public async Task InvokeAsync(HttpContext contexto)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var cuerpoOriginalRespuesta = contexto.Response.Body;
+                contexto.Response.Body = ms;
+
+                await siguiente(contexto);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                string respuesta = new StreamReader(ms).ReadToEnd();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                await ms.CopyToAsync(cuerpoOriginalRespuesta);
+                contexto.Response.Body = cuerpoOriginalRespuesta;
+
+                logger.LogInformation(respuesta);
+            }
+        }
+    }
+}
